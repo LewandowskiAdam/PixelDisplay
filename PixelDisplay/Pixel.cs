cc17826d4817560c8ef99138c95d5a360801f2d6
Adam Lewandowski <adam.lewandowski.tech@gmail.com>
2020-03-30 20:33:52 +0200

    Add project files.

---
+++ PixelDisplay/Pixel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+
+namespace PixelDisplay
+{
+    public partial class Pixel : Component
+    {
+        public Pixel()
+        {
+            InitializeComponent();
+        }
+
+        public Pixel(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
